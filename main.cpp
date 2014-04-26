57b6a18eb08dd98c0eaaa9b3380f198dbb1bf393
Egor <Egolior@mail.ru>
2014-04-26 22:42:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main()
 {
-    SingleLinkedList = singleLinkedList;
+    QueneOnSingleLinkedList = quene;
     Data data;
     int key = 0;
     do
@@ -23,10 +23,7 @@ int main()
         cout << " ~2~ - For delete element from the head " << endl;
         cout << " ~3~ - For show the Singly-Linked List " << endl;
         cout << " ~4~ - For clear the whole list " << endl;
-        cout << " ~5~ - For show element on the top " << endl;
-        cout << " ~6~ - For show element ob the tail endl;
-        cout << " ~7~ - For check the list is empty or not " << endl;
-        cout << " ~8~ - For show the length of the list " << endl;
+        cout << " ~5~ - For check the list is empty or not " << endl;
         cout << " ~0~ - For exit " << endl;
         cin >> key;
         switch( key )
@@ -35,41 +32,42 @@ int main()
         {
             system( " cls " );
             data = inputData( data );
-            singleLinkedList.push( data );
-            singleLinkedList.addSize( 1 );
+            quene.push( data );
         }
         break;
         case 2:
         {
             system( " cls " );
+            quene.pop( );
 
         }
         break;
         case 3:
         {
             system( " cls " );
+            quene.showQuene( );
         }
         break;
         case 4:
         {
             system( " cls " );
+            quene.clearQuene( );
+
         }
         break;
         case 5:
         {
             system( " cls " );
+             if( quene.queneIsEmpty( ) )
+            {
+                cout << " Quene isn't empty ";
+            }
+            else
+            {
+                cout << " Quene is empty ";
+            }
         }
         break;
-        case 6:
-        {
-            system( " cls " );
-        }
-        break;
-        case 7:
-        {
-            system( " cls " );
-        }
-        }
 
     }
     while( key );
