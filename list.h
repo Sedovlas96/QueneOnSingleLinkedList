e74a48894894acf547963d65a869581cecc3b0a7
Egor <Egolior@mail.ru>
2014-04-26 22:41:30 +0300

    Update list.h

--- list.h
+++ list.h
@@ -1,29 +1,26 @@
 #ifndef LIST_H
 #define LIST_H
+#include <iostream>
 
 typedef int Data;
 
-class SingleLinkedList
+class QueneOnSingleLinkedList
 {
-    public:
-        SingleLinkedList( );
-        ~SingleLinkedList( );
-        void push( Data datum );
-        Data pop( );
-        void showQuene( ) const;
-        void clearQuene( );
-        void showTop ( );
-        void showTail( );
-        bool isEmpty( ) const noexcept;
-        int showLengthOfQuene( ) const;
-        void addSize( int a );
-    private:
-        int lengthOfQuene;
-        struct Node
-        {
-            Data datum;
-            Node *link;
-        }; *tail, *top;
+public:
+    QueneOnSingleLinkedList( );
+    ~QueneOnSingleLinkedList( );
+    void push( Data datum );
+    Data pop( );
+    void showQuene( ) const;
+    void clearQuene( );
+    bool queneIsEmpty( ) const;
+
+private:
+    struct Node
+    {
+        Data datum;
+        Node *link;
+    } *top;
 
 };
 
